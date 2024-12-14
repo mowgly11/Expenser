670c68d3f841838b3440200cf11b2a3dbf9437bf
mowgly11 <oussamabouzalim55@gmail.com>
2024-12-14 23:04:44 +0100

    custom types for database usage

---
+++ types/databaseTypes.ts
@@ -0,0 +1,25 @@
+export type DataBaseOutput = {
+    id: string,
+    username: string,
+    expenses: Array<Expense> | [],
+    monthly_report: Array<Report> | []
+}
+
+export type Expense = {
+    item: string,
+    price: number,
+    date: string,
+    category: string,
+    picture: Buffer | null
+}
+
+export type Report = {
+    date: string,
+    total_spent: number,
+    imp_percentage: number
+}
+
+export type UserDescription = {
+    id: string,
+    username: string
+}
