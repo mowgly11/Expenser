cf0bd5257242649a6eaf7c15da397c1cf588c849
mowgly11 <oussamabouzalim55@gmail.com>
2024-12-20 19:46:32 +0100

    changed authentication to use JWT instead of sessions

--- passport.ts
+++ passport.ts
@@ -15,24 +15,19 @@ function initialisePassport(passport: PassportStatic, getUserById: (id: string) 
     }, async (_accessToken, _refreshToken, profile: Profile, done) => {
         let user = await getUserById(profile.id);
         
-        if (user) return done(null, profile);
+        if (user) return done(null, user);
 
         else {
             let make_user = await database.makeUser({
                 id: profile.id,
-                username: profile.name == null ? `john_${Date.now()}`: profile.name.givenName.trim().split(" ").join("_").toLowerCase()
+                username: profile.name == null ? `john_doe_${Date.now()}`: profile.name.givenName.trim().split(" ").join("_").toLowerCase()
             });
 
-            if(!make_user) return done(null, false, { message: "An Error Just Occured, Try Again Later" });
+            if(make_user !== false) return done(null, false, { message: "An Error Just Occured, Try Again Later" });
 
-            return done(null, profile);
+            return done(null, make_user);
         }
     }));
-
-    passport.serializeUser((user: any, done) => done(null, user.id));
-    passport.deserializeUser((id: any, done) => {
-        return done(null, getUserById(id));
-    });
 }
 
 export default initialisePassport;
