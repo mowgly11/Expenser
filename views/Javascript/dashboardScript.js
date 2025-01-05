0f720b0e513a097040db76b7a34b02fff1b8cabb
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-05 14:22:27 +0100

    added categories, client-side input validation & modified date format

--- views/Javascript/dashboardScript.js
+++ views/Javascript/dashboardScript.js
@@ -10,11 +10,14 @@ const alertElement = document.querySelector(".alert-popup");
 const alertElementTxt = document.querySelector(".alert-popup-m");
 const alertElementTitle = document.querySelector(".alert-popup-title");
 
+let datePattern = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+let categories = ["food", "clothing", "drinks", "bills", "transportation"]
+
 let inputs = [itemInput, priceInput, dateInput, categoryInput, picInput];
 
 // adding expenses n shit
 expenseForm.addEventListener("submit", async (ev) => {
-    showLoadingScreen()
+    showLoadingScreen();
     ev.preventDefault();
 
     let item = itemInput.value;
@@ -23,7 +26,11 @@ expenseForm.addEventListener("submit", async (ev) => {
     let category = categoryInput.value;
     let pic = picInput.value == '' ? null : picInput.value;
 
+    console.log(date)
+
     if(item.length > 50) return showAlertError("item is too bigger than 50 chars.");
+    if(datePattern.test(date) === false && date != null) return showAlertError("Invalid Date.");
+    if(categories.indexOf(category) === -1) return showAlertError("invalid category, try again.");
 
     let domain = new URL(window.location.href).origin;
     const addExpense = await fetch(`${domain}/add_expense`, {
@@ -54,6 +61,7 @@ expenseForm.addEventListener("submit", async (ev) => {
 
 let cooldown = false;
 function showAlertSuccess(text = "Successfully executed!", bg = 'bg-success', title = 'Success!') {
+    hideLoadingScreen();
     if (cooldown) return;
     alertElementTitle.textContent = title;
     alertElementTxt.innerHTML = text;
@@ -64,7 +72,7 @@ function showAlertSuccess(text = "Successfully executed!", bg = 'bg-success', ti
         alertElement.classList.remove("visible-alert");
         alertElement.classList.remove(bg);
         cooldown = false;
-    }, 5000);
+    }, 2000);
 }
 
 function showAlertError(text = "An Error Just Occured.", bg = 'bg-danger', title = 'Error!') {
