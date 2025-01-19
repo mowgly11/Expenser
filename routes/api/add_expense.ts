be27b04b46379841ad1b28c264ccbf64539b24f0
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-19 13:31:02 +0100

    improved readability of code

--- routes/api/add_expense.ts
+++ routes/api/add_expense.ts
@@ -2,6 +2,12 @@ import type { Request, Response, NextFunction } from 'express';
 import middleware from '../../middleware/auth_middleware.ts';
 import database from "../../Database/methods.ts";
 import type { Expense } from '../../types/databaseTypes.ts';
+import sanitize from 'sanitize-html';
+
+const sanitizeItem = (item: string) => sanitize(item, {
+  allowedTags: [], // Allow no HTML tags
+  allowedAttributes: {}, // Allow no attributes
+});
 
 const categories = ["food", "clothing", "drinks", "bills", "transportation"];
 
@@ -11,28 +17,30 @@ export default {
   middleware: middleware.checkAuthenticated,
   Post: async function (req: Request, res: Response, next: NextFunction) {
     let user: any = req.user;
-    const expenseDescription: Expense = req.body;
+    const { item, price, amount, date, category }: Expense = req.body;
 
     let datePattern: RegExp = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
 
-    if (expenseDescription.item.length > 50 || expenseDescription.item.length < 3) return res.json({ status: 'failed', message: "item is larger than 50 chars or less than 3 chars." });
-    if (typeof expenseDescription.price !== 'number' || isNaN(expenseDescription.price) || expenseDescription.price < 0) return res.json({ status: 'failed', message: "price must be a positive number." });
-    if (typeof expenseDescription.amount !== 'number' || isNaN(expenseDescription.amount) || !Number.isInteger(expenseDescription.amount) || expenseDescription.amount < 0) return res.json({ status: 'failed', message: "amount must be a positive number." });
-    if (datePattern.test(expenseDescription.date) === false && expenseDescription.date != null) return res.json({ status: 'failed', message: "Invalid date. Format should be yyyy-mm-dd" });
-    if (categories.indexOf(expenseDescription.category) === -1) return res.json({ status: 'failed', message: "Invalid category" });
+    const sanitizedItem = sanitizeItem(item);
+
+    if (typeof sanitizedItem !== 'string' || sanitizedItem.length > 50 || sanitizedItem.length < 3) return res.json({ status: 'failed', message: "item is larger than 50 chars or less than 3 chars." });
+    if (typeof price !== 'number' || isNaN(price) || price < 0 || price > 9999999999) return res.json({ status: 'failed', message: "price must be a positive number." });
+    if (typeof amount !== 'number' || isNaN(amount) || !Number.isInteger(amount) || amount < 0 || amount > 9999999999) return res.json({ status: 'failed', message: "amount must be a positive number." });
+    if (datePattern.test(date) === false && date != null) return res.json({ status: 'failed', message: "Invalid date. Format should be yyyy-mm-dd" });
+    if (categories.indexOf(category) === -1) return res.json({ status: 'failed', message: "Invalid category" });
 
     let added = await database.addExpense(
       user._doc.id,
       {
-        item: expenseDescription.item,
-        price: expenseDescription.price,
-        amount: expenseDescription.amount,
-        date: expenseDescription.date ?? getNewDate(),
-        category: expenseDescription.category,
-        picture: expenseDescription.picture ?? null
+        item,
+        price,
+        amount,
+        date: date ?? getNewDate(),
+        category
       });
 
     if (!added) return res.json({ status: 'failed', message: "there was an error, try again." });
+
     return res.json({ status: 'ok', message: "successfully added!" });
   }
 };
