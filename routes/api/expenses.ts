7d77d1749f38fb850581d8418b3ef000592f1c4a
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-19 13:31:45 +0100

    implemented the expenses method to return an array of expenses (with a max of 10 expenses each request)

--- routes/api/expenses.ts
+++ routes/api/expenses.ts
@@ -1,11 +1,20 @@
 import type { Request, Response, NextFunction } from 'express';
 import middleware from '../../middleware/auth_middleware.ts';
+import database from '../../Database/methods.ts';
 
 export default {
-  methods: ["get"],
+  methods: ["post"],
   endpoint: "/expenses",
   middleware: middleware.checkAuthenticated,
-  Get: async function(req: Request, res: Response, next: NextFunction) {
-    res.send("you are authenticated, id: ");
+  Post: async function (req: Request, res: Response, next: NextFunction) {
+    const amount: number = parseInt(req.body.amount);
+    const start: number = parseInt(req.body.start);
+    let user: any = req.user;
+
+    if (!Number.isInteger(amount) || !Number.isInteger(start) || amount > 10 || amount < 0 || start < 0) return res.json({ status: 'failed', message: 'amount and start should be positive integers and of type number' });
+
+    let data = await database.getUser(user._doc.id);
+
+    return res.json({ status: 'ok', message: data?.expenses.slice(start, start + amount) });
   }
 };
