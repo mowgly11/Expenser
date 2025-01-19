e67eb9fb0655b67d6f5e70260868513f5895ecdc
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-19 13:33:09 +0100

    implemented the expenses feature so all added expenses will be visible on the screen

--- views/Javascript/dashboardScript.js
+++ views/Javascript/dashboardScript.js
@@ -1,62 +1,104 @@
+"use strict";
+
 let expenseForm = document.getElementById("expenseForm");
+
+let expensesTemplate = document.getElementById("expense");
+let expensesHolder = document.getElementById("expenses-container");
 
 let itemInput = document.getElementById("item");
 let priceInput = document.getElementById("price");
 let amountInput = document.getElementById("amount");
 let dateInput = document.getElementById("date");
 let categoryInput = document.getElementById("category");
-let picInput = document.getElementById("pic");
 
 const alertElement = document.querySelector(".alert-popup");
 const alertElementTxt = document.querySelector(".alert-popup-m");
 const alertElementTitle = document.querySelector(".alert-popup-title");
 
 let datePattern = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-let categories = ["food", "clothing", "drinks", "bills", "transportation"]
+let categories = ["food", "clothing", "drinks", "bills", "transportation"];
 
-// adding expenses n shit
-expenseForm.addEventListener("submit", async (ev) => {
-    showLoadingScreen();
-    ev.preventDefault();
+let domain = new URL(window.location.href).origin;
 
-    let item = itemInput.value;
-    let price = parseInt(priceInput.value);
-    let amount = parseInt(amountInput.value);
-    let date = dateInput.value == '' ? null : dateInput.value;
-    let category = categoryInput.value;
-    let pic = picInput.value == '' ? null : picInput.value;
+document.addEventListener("DOMContentLoaded", async () => {
+    let getExpenses;
 
-    if(item.length > 50 || item.length < 3) return showAlertError("item is larger than 50 chars or less than 3 chars.");
-    if(datePattern.test(date) === false && date != null) return showAlertError("Invalid Date.");
-    if(categories.indexOf(category) === -1) return showAlertError("invalid category, try again.");
+    try {
+        getExpenses = await fetch(`${domain}/expenses`, {
+            method: "post",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                start: 0,
+                amount: 10
+            })
+        });
+    } catch (e) {
+        showAlertError("failed to get expenses, please try reloading your page.");
+    }
 
-    let domain = new URL(window.location.href).origin;
-    const addExpense = await fetch(`${domain}/add_expense`, {
-        method: "post",
-        headers: {
-            'Content-Type': "application/json",
-        },
-        body: JSON.stringify({
-            item,
-            price,
-            amount,
-            date,
-            category,
-            pic
-        })
+    getExpenses = await getExpenses.json();
+
+    if (getExpenses == null) showAlertError("failed to get expenses, lpease try reloading your page");
+
+    let getExpensesArray = getExpenses.message;
+
+    getExpensesArray.forEach(expense => {
+        let { item, price, amount, category, date } = expense;
+        const expenseClone = expensesTemplate.content.cloneNode(true);
+
+        expenseClone.querySelector("#show-item").textContent = item;
+        expenseClone.querySelector("#show-price").textContent = price;
+        expenseClone.querySelector("#show-amount").textContent = amount;
+        expenseClone.querySelector("#show-category").textContent = category;
+        expenseClone.querySelector("#show-date").textContent = date;
+
+        expensesHolder.append(expenseClone);
     });
 
-    if(addExpense.redirected) return showAlertError("an error just occured, Please reload your page.");
+    expenseForm.addEventListener("submit", async (ev) => {
+        showLoadingScreen();
+        ev.preventDefault();
 
-    let response = await addExpense.json();
-    hideLoadingScreen();
+        let item = itemInput.value;
+        let price = parseInt(priceInput.value);
+        let amount = parseInt(amountInput.value);
+        let date = dateInput.value == '' ? null : dateInput.value;
+        let category = categoryInput.value;
 
-    if (response.status == 'ok') {
-        showAlertSuccess(response.message)
-    } else {
-        showAlertError(response.message)
-    }
+        if (item.length > 50 || item.length < 3) return showAlertError("item is larger than 50 chars or less than 3 chars.");
+        if (datePattern.test(date) === false && date != null) return showAlertError("Invalid Date.");
+        if (categories.indexOf(category) === -1) return showAlertError("invalid category, try again.");
+
+        const addExpense = await fetch(`${domain}/add_expense`, {
+            method: "post",
+            headers: {
+                'Content-Type': "application/json",
+            },
+            body: JSON.stringify({
+                item,
+                price,
+                amount,
+                date,
+                category,
+            })
+        });
+
+        if (addExpense.redirected) return showAlertError("an error just occured, Please reload your page.");
+
+        let response = await addExpense.json();
+        hideLoadingScreen();
+
+        if (response.status == 'ok') {
+            showAlertSuccess(response.message)
+        } else {
+            showAlertError(response.message)
+        }
+    });
 });
+
+// adding expenses
 
 let cooldown = false;
 function showAlertSuccess(text = "Successfully executed!", bg = 'bg-success', title = 'Success!') {
@@ -75,7 +117,7 @@ function showAlertSuccess(text = "Successfully executed!", bg = 'bg-success', ti
 }
 
 function showAlertError(text = "An Error Just Occured.", bg = 'bg-danger', title = 'Error!') {
-    showAlertSuccess(text, bg, title)
+    showAlertSuccess(text, bg, title);
 }
 
 const loadingScreen = document.querySelector('.blur-overlay');
