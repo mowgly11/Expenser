503e7becf1e3f7ecbceb16b7b50efdc4359196b1
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-23 00:51:11 +0100

    created a new api endpoint for handling expenses deletion

---
+++ routes/api/delete_expense.ts
@@ -0,0 +1,22 @@
+import type { Request, Response, NextFunction } from 'express';
+import middleware from '../../middleware/auth_middleware.ts';
+import database from "../../Database/methods.ts";
+import type { Expense } from '../../types/databaseTypes.ts';
+
+export default {
+  methods: ["post"],
+  endpoint: "/delete_expense",
+  middleware: middleware.checkAuthenticated,
+  Post: async function (req: Request, res: Response, next: NextFunction) {
+    let user: any = req.user;
+    const { id } = req.body;
+
+    // add some checks later, i'm so fucking tired
+
+    let removeExpense: boolean = await database.removeItemById(user._doc.id, id, "expense");
+
+    if (!removeExpense) return res.json({ status: 'failed', message: "there was an error, try again." });
+
+    return res.json({ status: 'ok', message: "successfully added!" });
+  }
+};
