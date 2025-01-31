a18bcc169a3063bc7f5b9117fe65bd461d766b88
mowgly11 <oussamabouzalim55@gmail.com>
2025-01-31 16:02:13 +0100

    modified the HTTP methods from get to post

--- routes/api/reports.ts
+++ routes/api/reports.ts
@@ -2,10 +2,10 @@ import type { Request, Response, NextFunction } from 'express';
 import middleware from '../../middleware/auth_middleware.ts';
 
 export default {
-  methods: ["get"],
-  endpoint: "/reports",
+  methods: ["post"],
+  endpoint: "/api/reports",
   middleware: middleware.checkAuthenticated,
-  Get: async function(req: Request, res: Response, next: NextFunction) {
+  Post: async function(req: Request, res: Response, next: NextFunction) {
     res.send("you are authenticated, id: ");
   }
 };
